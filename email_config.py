1c62a5562af6dae404c6f1ffb1c432ecaffccc48
poojitha-kasani <poojithakasani11@gmail.com>
2025-06-26 21:59:03 +0000

    added nodemailer here

---
+++ email_config.py
@@ -0,0 +1,15 @@
+# email_config.py
+
+EMAIL_ADDRESS = 'poojithakasani11@gmail.com'  # Your Gmail address
+EMAIL_PASSWORD = 'fdwgeizhuesohcts'
+EMAIL_SUBJECT = 'Scheduled Email'
+EMAIL_BODY = '''
+Hello mam,
+
+This is the scheduled email.
+
+Regards,
+poojitha 🚀
+'''
+
+TO_EMAIL = 'uniquestunner92@gmail.com'  # Receiver's email
